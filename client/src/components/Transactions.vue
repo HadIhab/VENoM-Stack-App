4ffb47342812afb37ab721a026f52a15c85ba7c9
HadIhab <chocovani101@hotmail.fr>
2021-04-21 21:56:58 +0100

    Getting transactions from rest API++

--- client/src/components/Transactions.vue
+++ client/src/components/Transactions.vue
@@ -1,158 +1,145 @@
-import Vue from 'vue'
+<template>
+  <v-card style="width: 100%;">
+    <v-card-title>
+      <span class="pr-3">
+        Transactions for {{ months[currentMonth].name }} - {{ currentYear }}
+      </span>
+      <v-btn flat icon class="pr-2" v-on:click="gotoMonth(-1)">
+        <v-icon>keyboard_arrow_left</v-icon>
+      </v-btn>
+      <v-btn flat icon class="pr-2" v-on:click="gotoCurrentMonth">
+        <v-icon>today</v-icon>
+      </v-btn>
+      <v-btn flat icon v-on:click="gotoMonth(1)">
+        <v-icon>keyboard_arrow_right</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-text-field
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        v-model="search"
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+        v-bind:headers="headers"
+        v-bind:items="items"
+        v-bind:search="search"
+        item-key="_id"
+      >
+      <template slot="items" slot-scope="props">
+        <tr>
+          <td>
+            <v-edit-dialog
+              lazy
+            > {{ props.item.transactionDate }}
+              <v-text-field
+                slot="input"
+                label="Edit"
+                v-model="props.item.transactionDate"
+                single-line
+              ></v-text-field>
+            </v-edit-dialog>
+          </td>
+          <td class="text-xs-left">{{ props.item.transactionType }}</td>
+          <td class="text-xs-left" @click="props.expanded = !props.expanded">
+            {{ props.item.description }}
+          </td>
+          <td class="text-xs-right">{{ props.item.charge }}</td>
+          <td class="text-xs-right">{{ props.item.deposit }}</td>
+          <td class="text-xs-right">{{ props.item.balance }}</td>
+        </tr>
+      </template>
 
-const state = {
-  months: [
-    { name: 'Zero', abrev: 'ZZZ', index: 0 },
-    { name: 'January', abrev: 'Jan', index: 1 },
-    { name: 'February', abrev: 'Feb', index: 2 },
-    { name: 'March', abrev: 'Mar', index: 3 },
-    { name: 'April', abrev: 'Apr', index: 4 },
-    { name: 'May', abrev: 'May', index: 5 },
-    { name: 'June', abrev: 'Jun', index: 6 },
-    { name: 'July', abrev: 'Jul', index: 7 },
-    { name: 'August', abrev: 'Aug', index: 8 },
-    { name: 'September', abrev: 'Sep', index: 9 },
-    { name: 'October', abrev: 'October', index: 10 },
-    { name: 'November', abrev: 'Nov', index: 11 },
-    { name: 'December', abrev: 'Dec', index: 12 }
-  ],
-  currentMonth: 5,
-  currentYear: 2018,
-  transactions: [],
-  balanceCharges: 0,
-  balanceDeposits: 0
-}
+      <template slot="expand" slot-scope="props">
+        <v-card flat class="pl-5">
+          <v-card-text class="text-xs-left">
+            <v-text-field label="Note" v-model="props.item.notes"></v-text-field>
+          </v-card-text>
+        </v-card>
+      </template>
 
-const getters = {
-  transactionsByMonth: state => state.transactions,
-  balanceCharges: state => state.balanceCharges,
-  balanceDeposits: state => state.balanceCharges
-}
+      <template slot="pageText" slot-scope="{ pageStart, pageStop }">
+        From {{ pageStart }} to {{ pageStop }}
+      </template>
 
-const actions = {
-  getTransactionsByMonth ({ commit, state, rootState }, payload) {
-    // Make API call... Pass in selected Month and Year + UserId in hearder...
-    // Once transaction data is retrieved... commit the mutation to update state...
-    Vue.axios.get('/transaction/' + state.currentYear + '/' + state.currentMonth,
-      {headers: { 'userId': rootState.user.userId }})
-      .then((resp) => {
-        let data = resp.data
-        if (data && data.length > 0) {
-          commit('transactionsByMonth', data)
-        }
-      })
-      .catch((err) => {
-        console.log('Darn! There was an error getting transactions by month: ' + err)
-      })
-  },
-  getPreviousMonthsBalances ({ commit, state, rootState }, payload) {
-    commit('transactionsByMonth', [])
-    // Make API call... Pass in selected Month and Year + UserId in hearder...
-    Vue.axios.get('/transaction/balance/' + state.currentYear + '/' + state.currentMonth,
-      {headers: { 'userId': rootState.user.userId }})
-      .then((resp) => {
-        console.log('GET transaction/balance', resp)
-        let data = resp.data
-        if (data && data.length > 0) {
-          commit('balanceCharges', data[0].charges)
-          commit('balanceDeposits', data[0].deposits)
-        } else {
-          commit('balanceCharges', 0)
-          commit('balanceDeposits', 0)
-        }
-      })
-      .catch((err) => {
-        console.log('Darn! There was an error getting balances: ' + err)
-      })
-  },
-  async gotoMonth ({ commit }, increment) {
-    commit('gotoMonth', increment)
-  },
-  async gotoCurrentMonth ({ commit }) {
-    commit('gotoCurrentMonth')
-  }
-}
+    </v-data-table>
+  </v-card>
+</template>
 
-const mutations = {
-  transactionsByMonth (state, data) {
-    // Start by clearing the array...
-    state.transactions = []
-
-    if (data && data.length > 0) {
-      data.forEach(tx => {
-        state.transactions.push(mapTransaction(tx, state))
-      })
-    }
-    console.log('Transactions by month mutated: ', state.transactions)
-  },
-  balanceCharges (state, data) {
-    state.balanceCharges = data
-  },
-  balanceDeposits (state, data) {
-    state.balanceDeposits = data
-  },
-  gotoMonth (state, increment) {
-    let newMonth = state.currentMonth += increment
-    // Sanity checks now...
-    if (newMonth > 12) {
-      newMonth = 1
-      state.currentYear += 1
-    } else if (newMonth < 1) {
-      newMonth = 12
-      state.currentYear -= 1
-    }
-
-    state.currentMonth = newMonth
-  },
-  gotoCurrentMonth (state) {
-    let dt = new Date(Date.now())
-    state.currentMonth = dt.getUTCMonth() + 1
-    state.currentYear = dt.getUTCFullYear()
-  }
-}
-
-// Helper functions section ------------------------------
-
-function mapTransaction (tx, state) {
-  const transDate = new Date(tx.transactionDate)
-  const months = state.months
-  let transaction = {
-    transactionDate: months[transDate.getUTCMonth() + 1].abrev + '-' + transDate.getUTCDate(),
-    transactionType: tx.transactionType,
-    description: tx.description,
-    charge: moneyFormatter(tx.charge),
-    deposit: moneyFormatter(tx.deposit),
-    balance: moneyFormatter(calcRunningBalance(tx, state))
-  }
-  return transaction
-}
-
-function moneyFormatter (amount) {
-  let formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-    // the default value for minimumFractionDigits depends on the currency
-    // and is usually already 2
-  })
-
-  return formatter.format(amount)
-}
-
-function calcRunningBalance (tx, state) {
-  // any new charges?
-  if (tx && tx.charge > 0) {
-    state.balanceCharges += tx.charge
-  } else if (tx && tx.deposit > 0) {
-    state.balanceDeposits += tx.deposit
-  }
-
-  return state.balanceCharges - state.balanceDeposits
-}
-
+<script>
 export default {
-  state,
-  getters,
-  actions,
-  mutations
+  computed: {
+    currentMonth () {
+      return this.$store.state.transactions.currentMonth
+    },
+    currentYear () {
+      return this.$store.state.transactions.currentYear
+    },
+    months () {
+      return this.$store.state.transactions.months
+    },
+    items () {
+      return this.$store.getters.transactionsByMonth
+    },
+    balanceCharges () {
+      return this.$store.getters.balanceCharges
+    },
+    balanceDeposits () {
+      return this.$store.getters.balanceDeposits
+    }
+  },
+  data () {
+    return {
+      max25chars: (v) => v.length <= 25 || 'Input too long!',
+      search: '',
+      pagination: {},
+      headers: [
+        { text: 'Date', align: 'center', sortable: false, value: 'date' },
+        { text: 'Type', align: 'center', sortable: false, value: 'type' },
+        { text: 'Description', align: 'center', sortable: false, value: 'description' },
+        { text: 'Charge (-)', align: 'center', sortable: false, value: 'paymentAmt' },
+        { text: 'Deposit (+)', align: 'center', sortable: false, value: 'depositAmt' },
+        { text: 'Balance', align: 'center', sortable: false, value: 'balance' }
+      ]
+    }
+  },
+  methods: {
+    getTransactionsByMonth: function () {
+      this.$store.dispatch('getTransactionsByMonth')
+    },
+    getPreviousMonthsBalances: function () {
+      this.$store.dispatch('getPreviousMonthsBalances')
+    },
+    gotoMonth: function (increment) {
+      this.$store.dispatch('gotoMonth').then(() => {
+        // Load selected month transaction data now...
+        this.getPreviousMonthsBalances()
+        this.getTransactionsByMonth()
+      })
+    },
+    gotoCurrentMonth: function () {
+      this.$store.dispatch('gotoCurrentMonth').then(() => {
+        // Load selected month transaction data now...
+        this.getPreviousMonthsBalances()
+        this.getTransactionsByMonth()
+      })
+    }
+  },
+  mounted: async function () {
+    // const me = this
+    await this.getPreviousMonthsBalances()
+    await console.log(this.balanceCharges)
+    await console.log(this.balanceDeposits)
+
+    await this.getTransactionsByMonth()
+  }
 }
+</script>
+
+<style scoped>
+    .transactions {
+        width: 100%;
+    }
+</style>
